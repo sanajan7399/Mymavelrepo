55ff504472eb7978b6770b64e2a4cfc536666329
user <user@DESKTOP-ISDQQNT>
2023-09-12 12:30:51 +0530

    my first commitment

---
+++ mymavenproject/src/main/java/mymavenproject/New.java
@@ -0,0 +1,14 @@
+package mymavenproject;
+
+public class New {
+public void name()
+{
+	System.out.println("Nimmy");
+}
+public static void main(String args[])
+{
+	New obj = new New();
+	obj.name();
+	
+}
+}
